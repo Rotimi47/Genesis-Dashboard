8c60ab9dfddb638fabcbaca207e10b9a970a859a
Omoyeni Oluwadurotimi <oreomoyeni01@gmail.com>
2026-08-19 14:57:40 +0100

    initialize Genesis app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Genesis-Dashboard
 Genesis manages businesses, customers, transactions, transfers and internal operations.
+
+
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+       <div className="flex min-h-screen items-center justify-center bg-slate-950">
+      <h1 className="text-5xl font-bold text-white">
+        Genesis
+      </h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
